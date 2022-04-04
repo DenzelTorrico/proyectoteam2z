a39d26d99cfad066ce416bbbe85ff066fa29c796
saulol1 <saulolima2502@gmail.com>
2022-04-04 14:57:59 -0500

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO</a>
                         @endif
                     @endauth
                 </div>
@@ -55,8 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
-                                </div>
+                                NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO                                </div>
                             </div>
                         </div>
 
