5bf7e15abb33b15291335875bfdcdf467df39fc4
Ronaldo Enciso <ronaldoencisoluque@gmail.com>
2022-04-11 16:08:45 -0500

    refactor: layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    @extends("layouts.footer")
-</head>
-<body>
-    
-    @section("footer")
-    @endsection
-</body>
-</html>
+@extends("layouts.master")
+@section('slot')
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus autem sit optio dicta quam voluptas non. Aliquam accusamus doloribus ad magni nam dolorum aspernatur quidem, quasi est omnis voluptates ab magnam neque ut, itaque qui facere aliquid hic temporibus eos reiciendis? Quam incidunt eius mollitia, assumenda tempore veniam tempora ipsum ipsam nisi maiores possimus quasi fugit dolor obcaecati libero laboriosam cum repellat alias odit exercitationem ab. Cumque vel quas ex voluptatem debitis inventore, fuga totam, odit earum quidem atque. Aliquam labore assumenda aut illo eaque, nisi nulla ullam aspernatur iure facere, accusamus numquam, magni voluptatum distinctio fugiat? Cum, veniam vero.</p>
+@endsection
