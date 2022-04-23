492554122af40082cedcce35a24c0fcb0efa07b1
Denzel <denzel_silva@Torrico.com>
2022-04-23 13:20:30 -0500

    add authenticated and middlewarev2

--- resources/views/welcome.blade.php
+++
@@ -1,6 +0,0 @@
-@extends("layouts.master")
-<script src="https://cdn.tailwindcss.com"></script>
-@section('slot')
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam natus aliquam vitae consequuntur consectetur obcaecati ducimus veritatis rem soluta corrupti vero accusantium temporibus assumenda harum aliquid, ea tempore sunt ipsa.</p>
-@endsection
-
--- routes/web.php
+++ routes/web.php
@@ -14,21 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/s', function () {
-    return view('saludos');
-});
-Route::get('/login', function () {
-    return view('login');
-});*/
 Route::get('/login', [Controller::class, 'login'])->middleware("guest");
 Route::post('/logeado', [Controller::class, 'logeado']);
 Route::get('/register', [Controller::class, 'register'])->middleware("guest");
 Route::post('/registrado', [Controller::class, 'registrado']);
 Route::get("/welcome", [Controller::class, 'welcome'])/*->middleware('auth')*/;
 Route::get("/logout",[Controller::class, 'logout']);
-/*Route::get('/terminos', function () {
-    return view('terminos');
-});
-Route::get('/register', function () {
-    return view('register');
-});
