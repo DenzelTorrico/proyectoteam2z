0ad203e604db315f4a2997601bb274f961debb82
Ronaldo Enciso <ronaldoencisoluque@gmail.com>
2022-04-26 15:05:58 -0500

    Update view index

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,6 +8,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.1/dist/js/splide.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.1/dist/css/splide.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <title>Document</title>
 </head>
 <body>
