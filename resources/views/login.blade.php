333f9de9e19921316568a4f49dba3e4a63ddab2f
Ronaldo Enciso <ronaldoencisoluque@gmail.com>
2022-04-28 16:35:27 -0500

    feat: add new dependecies prettier-blade

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,31 +1,33 @@
 @extends("layouts.master")
 <script src="https://cdn.tailwindcss.com"></script>
 @section('slot')
-<form action="{{ route('logeado') }}" class="flex flex-col  items-center mt-10 pb-3" method="post">
-     @csrf
-     <input type="hidden" name="_method" value="POST" />
-    <div>
-        <img class="h-20" src="{{url('image/user.png')}}"/>
-    </div>
+    <form action="{{ route('logeado') }}" class="flex flex-col  items-center mt-10 pb-3" method="post">
+        @csrf
+        <input type="hidden" name="_method" value="POST" />
+        <div>
+            <img class="h-20" src="{{ url('image/user.png') }}" />
+        </div>
 
-   <div class="flex flex-col w-1/4 mt-5">
-    <label class="text-gray-700 text-sm font-bold mb-2" for="">Correo</label>
-    <input class="shadow border rounded focus:outline-none  py-2 px-3" type="text" name="correo" id="" placeholder="email">
-     @error("correo")
-     <div class="text-red-500">{{ $message }}</div>
-     @enderror
-</div>
-   
-   <div class="flex flex-col w-1/4 mt-5">
-        <label class="text-gray-700 text-sm font-bold mb-2" for="">Contraseña</label>
-        <input class="shadow rounded border px-2 py-2 focus:outline-none" type="password" name="password" id="" placeholder="Contraseña">
-   </div>
-    
-   <div class="flex flex-col mt-5"> 
-        <input class="block text-sm text-white bg-blue-500 p-2 rounded" type="submit" value="Ingresar">
-        <a class="mt-5 text-blue-500" href="">¿Olvidaste la contraseña?</a>
-   </div>
+        <div class="flex flex-col w-1/4 mt-5">
+            <label class="text-gray-700 text-sm font-bold mb-2" for="">Correo</label>
+            <input class="shadow border rounded focus:outline-none  py-2 px-3" type="text" name="correo" id=""
+                placeholder="email">
+            @error('correo')
+                <div class="text-red-500">{{ $message }}</div>
+            @enderror
+        </div>
 
-    
-</form>
+        <div class="flex flex-col w-1/4 mt-5">
+            <label class="text-gray-700 text-sm font-bold mb-2" for="">Contraseña</label>
+            <input class="shadow rounded border px-2 py-2 focus:outline-none" type="password" name="password" id=""
+                placeholder="Contraseña">
+        </div>
+
+        <div class="flex flex-col mt-5">
+            <input class="block text-sm text-white bg-blue-500 p-2 rounded" type="submit" value="Ingresar">
+            <a class="mt-5 text-blue-500" href="">¿Olvidaste la contraseña?</a>
+        </div>
+
+
+    </form>
 @endsection
--- resources/views/product/createProduct.blade.php
+++ resources/views/product/createProduct.blade.php
@@ -1,30 +1,35 @@
 @extends('layouts.master')
 
 @section('slot')
-
-<h5>Categoría</h5>
+    <h5>Categoría</h5>
 
     <form method="POST" action="{{ route('product.save') }}">
         @csrf
         <label>Nombre</label>
-        <input name="nombre" type="text" class="border-gray-600/50 py-1 px-2 rounded-md outline-none border-[1px] w-full" aria-label="nombre" value="{{ old('nombre') }}">
+        <input name="nombre" type="text" class="border-gray-600/50 py-1 px-2 rounded-md outline-none border-[1px] w-full"
+            aria-label="nombre" value="{{ old('nombre') }}">
         @error('nombre')
-            <span class="text-red-500 text-sm">{{$message}}</span><br>
+            <span class="text-red-500 text-sm">{{ $message }}</span><br>
         @enderror
         <label>Descripcion</label>
-        <textarea name="descripcion" rows="5" class="border-gray-600/50 py-1 px-2 rounded-md outline-none border-[1px] w-full" aria-label="descripcion">{{ old('descripcion') }}</textarea>
+        <textarea name="descripcion" rows="5" class="border-gray-600/50 py-1 px-2 rounded-md outline-none border-[1px] w-full"
+            aria-label="descripcion">{{ old('descripcion') }}</textarea>
         @error('descripcion')
-            <span class="text-red-500 text-sm">{{$message}}</span><br>
+            <span class="text-red-500 text-sm">{{ $message }}</span><br>
         @enderror
         <label>Precio</label>
-        <input name="precio" type="number" min="0" step="0.5" class="border-gray-600/50 py-1 px-2 rounded-md outline-none border-[1px] w-full" aria-label="precio" value="{{ old('precio') }}">
+        <input name="precio" type="number" min="0" step="0.5"
+            class="border-gray-600/50 py-1 px-2 rounded-md outline-none border-[1px] w-full" aria-label="precio"
+            value="{{ old('precio') }}">
         @error('precio')
-            <span class="text-red-500 text-sm">{{$message}}</span><br>
+            <span class="text-red-500 text-sm">{{ $message }}</span><br>
         @enderror
         <label>Descuento</label>
-        <input name="descuento" type="number" min="0" step="0.5" class="border-gray-600/50 py-1 px-2 rounded-md outline-none border-[1px] w-full" aria-label="descuento" value="{{ old('descuento') }}">
+        <input name="descuento" type="number" min="0" step="0.5"
+            class="border-gray-600/50 py-1 px-2 rounded-md outline-none border-[1px] w-full" aria-label="descuento"
+            value="{{ old('descuento') }}">
         @error('descuento')
-            <span class="text-red-500 text-sm">{{$message}}</span><br>
+            <span class="text-red-500 text-sm">{{ $message }}</span><br>
         @enderror
         <label>Estado</label>
         <select name="estadoProducto">
@@ -35,30 +40,32 @@
             <option value="reparado">Reparado</option>
         </select>
         @error('estadoProducto')
-            <span class="text-red-500 text-sm">{{$message}}</span><br>
+            <span class="text-red-500 text-sm">{{ $message }}</span><br>
         @enderror
         <label>Stock</label>
-        <input name="stock" type="number" min="0" step="1" class="border-gray-600/50 py-1 px-2 rounded-md outline-none border-[1px] w-full" aria-label="stock" value="{{ old('stock') }}">
+        <input name="stock" type="number" min="0" step="1"
+            class="border-gray-600/50 py-1 px-2 rounded-md outline-none border-[1px] w-full" aria-label="stock"
+            value="{{ old('stock') }}">
         @error('stock')
-            <span class="text-red-500 text-sm">{{$message}}</span><br>
+            <span class="text-red-500 text-sm">{{ $message }}</span><br>
         @enderror
         <label>Foto</label>
-        <input name="foto" type="text" class="border-gray-600/50 py-1 px-2 rounded-md outline-none border-[1px] w-full" aria-label="foto" value="{{ old('foto') }}">
+        <input name="foto" type="text" class="border-gray-600/50 py-1 px-2 rounded-md outline-none border-[1px] w-full"
+            aria-label="foto" value="{{ old('foto') }}">
         @error('foto')
-            <span class="text-red-500 text-sm">{{$message}}</span><br>
+            <span class="text-red-500 text-sm">{{ $message }}</span><br>
         @enderror
         <label>Categoría</label>
         <select name="idcategoria">
             <option value=""></option>
             @foreach ($categories as $categoria)
-                <option value="{{ $categoria->id }}">{{ $categoria->nombre}}</option>
+                <option value="{{ $categoria->id }}">{{ $categoria->nombre }}</option>
             @endforeach
         </select>
         @error('idcategoria')
-            <span class="text-red-500">{{$message}}</span><br>
+            <span class="text-red-500">{{ $message }}</span><br>
         @enderror
 
-        <input type="submit" class="bg-blue-400 py-1 px-3 rounded-md text-white" value="Enviar"/>
+        <input type="submit" class="bg-blue-400 py-1 px-3 rounded-md text-white" value="Enviar" />
     </form>
-
 @endsection
