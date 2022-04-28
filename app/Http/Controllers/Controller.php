f9398c8ec008320219113f631af8fd7ca705c9b9
Denzel <denzel_silva@Torrico.com>
2022-04-28 16:45:17 -0500

    feat: add category and filter

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -23,11 +23,32 @@ class Controller extends BaseController
     public function login(){
         return view('login');
     }
+    public function searchprice(Request $request){
+        $categories = Categories::all();
+        $buscador = $request->buscador;
+        $precio = $request->ranges;
+        $buscar = Productos::where("nombre","LIKE","%$buscador%")->where("precio","<","$precio")->get();
+        //return $precio;
+        return view('search',compact("buscar","categories","buscador"));
+    }
+    public function searchpricecategory(Request $request){
+        $categories = Categories::all();
+        $buscador = $request->id;
+        $precio = $request->ranges;
+        $buscar = Productos::where('idcategoria',"$buscador")->where("precio","<","$precio")->get();
+        return view("searchpricecategory",compact("categories","buscar","buscador"));
+    }
+    public function searchcategory(Request $request){
+        $categories = Categories::all();
+        $buscador = $request->categoria;
+        $buscar = Productos::where('idcategoria',"$buscador")->get();
+        return view('searchcategory',compact("categories","buscar","buscador"));
+    }
     public function search(Request $request){
         $categories = Categories::all();
         $buscador = $request->buscador;
         $buscar = Productos::where("nombre","LIKE","%$buscador%")->get();
-        return view('search',compact("buscar","categories"));
+        return view('search',compact("buscar","categories","buscador"));
     }
     public function logeado(Request $request){
        /* $nombre = $request->usuario;
--- resources/views/searchpricecategory.blade.php
+++ resources/views/searchpricecategory.blade.php
@@ -11,9 +11,9 @@
     
      <p>Productos menores a: </p>
     
-    <form action="{{route('searchprice')}}">
+    <form action="{{route('searchpricecategory')}}">
     <input type="range" value="100" name="ranges" min="100" max="8000" step="100" onchange="document.getElementById('range').innerHTML=this.value" id="">
-    <input type="hidden" name="buscador" value={{$buscador}}>
+    <input type="hidden" name="id" value={{$buscador}}>
    <p>S/<span id="range">100</span></p>
  
   <button type="submit" class="bg-blue-600 text-white rounded-lg p-2">></button>
