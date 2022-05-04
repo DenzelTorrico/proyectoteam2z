ad17d3e97da6c8e477f5a1a5eea6a85a193f8338
Denzel <denzel_silva@Torrico.com>
2022-05-04 15:10:04 -0500

    remove factory y add product seed

--- database/migrations/2022_04_19_190658_create_productos_table.php
+++ database/migrations/2022_04_19_190658_create_productos_table.php
@@ -16,7 +16,7 @@ class CreateProductosTable extends Migration
         Schema::create('productos', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
-            $table->string('descripcion');
+            $table->string('descripcion',500);
             $table->double('precio');
             $table->double('descuento');
             $table->string('estadoProducto');
